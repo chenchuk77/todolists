c7be3872deace8bdbfa9424c0129f183dbdef9c4
chenchuk <chenchuk@gmail.com>
2018-09-30 13:31:47 +0300

    pass rq12: cascading deletion from user ( profile/todolists )

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  has_one :profile
+  has_one :profile, dependent: :destroy
   has_many :todo_lists, dependent: :destroy
   has_many :todo_items, through: :todo_lists, source: :todo_items
 
   validates :username, presence: true
-  
+
 end
