87d4eb439f3ae69b86dc4bea26cd89859fd62773
chenchuk <chenchuk@gmail.com>
2018-10-06 14:21:20 +0300

    mod4: pass rq02

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true
 
+  has_secure_password
+
   def get_completed_count
   	# self inside a method refers to this object
   	self.todo_items.where(completed: true).count
