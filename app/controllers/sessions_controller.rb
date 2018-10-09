47e4c4743be8893a1709fca33302b5e3f2884937
chenchuk <chenchuk@gmail.com>
2018-10-09 23:44:32 +0300

    mod4: auth. pass rq11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_login, only: [:new, :create]
+  
   def new
   end
 
   def create
   	user = User.find_by(username: params[:user][:username])
   	password = params[:user][:password]
-
+  	puts "********** #{user}"
+  	puts "********** #{password}"
     if user && user.authenticate(password)
-      session[:user] = user.id
+      puts "********** authenticated"
+      session[:user_id] = user.id
       redirect_to root_path, notice: "Logged in successfully"
     else
+      puts "********** rejected"
       redirect_to login_path, alert: "Invalid username/password"
     end
   end
