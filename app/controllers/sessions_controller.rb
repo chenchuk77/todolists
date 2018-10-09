d2cd4e90eb85ef39ce88d4e88680d5361bc21c91
chenchuk <chenchuk@gmail.com>
2018-10-09 23:25:46 +0300

    mod4: pass rq10c, authentication via sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(username: params[:user][:username])
+  	password = params[:user][:password]
+
+    if user && user.authenticate(password)
+      session[:user] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      redirect_to login_path, alert: "Invalid username/password"
+    end
   end
 
   def destroy
   end
 end
+
+
+
+
+
+
+
+
+
